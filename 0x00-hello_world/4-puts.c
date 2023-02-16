ccaed06070fb3fe3b4c30a6a90a703ec5fcf7a02
yosri boubaker <yosribbk1996@gmail.com>
2023-02-16 15:23:58 -0800

    commit for task 4 prints a message using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints a message using puts'
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+{	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
