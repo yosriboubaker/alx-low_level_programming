6b7765839f16e107de8a650dc397fa848e7aee65
yosri boubaker <yosribbk1996@gmail.com>
2023-02-17 11:37:41 -0800

    second commit task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,14 +10,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if ( n > 0 ){
-		printf("%d is positive\n",n);
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
 	}
-	else if ( n < 0 ){
-		printf("%d is negative\n",n);
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
 	}
-	else {
-		printf("%d is zero\n",n);
-	}	
+	else
+	{
+		printf("%d is zero\n", n);
+	}
 	return (0);
 }
