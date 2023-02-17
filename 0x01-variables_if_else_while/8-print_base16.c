de5d89a61826a7004a4bbfb5bb5b90b3a435b62a
yosri boubaker <yosribbk1996@gmail.com>
2023-02-17 10:51:22 -0800

    first commit task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+	int i;
+
+	for(i = 0 ; i <= 9 ; i++){
+                putchar(i + '0');
+        }
+	for(alphabet = 'a' ; alphabet <= 'f' ; alphabet++){
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
