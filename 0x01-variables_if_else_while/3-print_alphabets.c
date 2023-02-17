1ed1c7b0b417f500283f59da19be505fd59ce305
yosri boubaker <yosribbk1996@gmail.com>
2023-02-17 11:53:45 -0800

    second commit task 1

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,14 @@ int main(void)
 {
 	char alphabet;
 
-	for(alphabet = 'a' ; alphabet <= 'z' ; alphabet++){
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	{
 		putchar(alphabet);
 	}
-	for(alphabet = 'A' ; alphabet <= 'Z' ; alphabet++){
-                putchar(alphabet);
-        }
+	for (alphabet = 'A' ; alphabet <= 'Z' ; alphabet++)
+	{
+	putchar(alphabet);
+	}
 	putchar('\n');
 	return (0);
 }
