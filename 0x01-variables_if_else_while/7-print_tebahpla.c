c6571c02e2a4d6932137d12bd879b0b97560a343
yosri boubaker <yosribbk1996@gmail.com>
2023-02-17 12:16:01 -0800

    second commit task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,7 +8,8 @@ int main(void)
 {
 	char alphabet;
 
-	for(alphabet = 'z' ; alphabet >= 'a' ; alphabet--){
+	for (alphabet = 'z' ; alphabet >= 'a' ; alphabet--)
+	{
 		putchar(alphabet);
 	}
 	putchar('\n');
