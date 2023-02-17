852043420de6d44635a4afefbe85ddcad14eae1b
yosri boubaker <yosribbk1996@gmail.com>
2023-02-17 09:51:01 -0800

    commit 1 task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for(alphabet = 'a' ; alphabet <= 'z' ; alphabet++){
+		putchar(alphabet);
+	}
+	for(alphabet = 'A' ; alphabet <= 'Z' ; alphabet++){
+                putchar(alphabet);
+        }
+	putchar('\n');
+	return (0);
+}
