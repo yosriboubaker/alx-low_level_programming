609aea7ed726b20322c767a6baa9261dcc4f5aa7
yosri boubaker <yosribbk1996@gmail.com>
2023-02-17 09:06:08 -0800

    third commit task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for(alphabet = 'a' ; alphabet <= 'z' ; alphabet++){
+		putchar(alphabet);
+	}
+	return (0);
+}
