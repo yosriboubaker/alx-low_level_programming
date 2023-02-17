ba3372e0b4266b5b139ef05cdc47843128951d74
yosri boubaker <yosribbk1996@gmail.com>
2023-02-17 11:03:57 -0800

    first commit task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for( i = 0 ; i <= 9 ; i++){
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
