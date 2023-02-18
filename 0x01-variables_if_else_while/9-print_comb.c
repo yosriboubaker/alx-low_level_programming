988092c94ea71fac2e2aeda8062046ac169804de
yosri boubaker <yosribbk1996@gmail.com>
2023-02-17 16:12:16 -0800

    second commit task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,14 @@ int main(void)
 {
 	int i;
 
-	for( i = 0 ; i <= 9 ; i++){
+	for (i = 0 ; i <= 9 ; i++)
+	{
 		putchar(i + '0');
-	  if(i != 9){	
+		if (i != 9)
+		{
 		putchar(',');
 		putchar(' ');
-	}
+		}
 	}
 	putchar('\n');
 	return (0);
