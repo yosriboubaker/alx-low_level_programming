500b47ed319fefb4b9c6ce2c1a3ff0677b164701
yosri boubaker <yosribbk1996@gmail.com>
2023-02-17 16:16:20 -0800

    second commit task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,13 +14,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	ld = ( n % 10 );
-	if ( ld > 5 ){
+	if ( ld > 5 )
+	{
 		printf("last digit of  %d is %d and is greater than 5\n",n,ld);
 	}
-	else if ( ld < 6 && ld != 0 ){
+	else if ( ld < 6 && ld != 0 )
+	{
 		printf("last digit of %d is %d and is less than 6 and not 0\n",n,ld);
 	}
-	else {
+	else
+	{
 		printf("last digit of %d is 0 and is 0\n",n);
 	}
 	return (0);
