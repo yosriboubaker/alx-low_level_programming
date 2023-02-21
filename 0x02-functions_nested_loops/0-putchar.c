f3ece0fed2d9d57210cbade74219779e3298cc4f
yosri boubaker <yosribbk1996@gmail.com>
2023-02-21 08:56:02 -0800

    first commit task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,16 +7,15 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void) 
+int main(void)
 {
-	char str[8]= "_putchar";
+	char str[8] = "_putchar";
 	int i;
 
-    	for (i = 0; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-        	putchar(str[i]);
-     	}
+		putchar(str[i]);
+	}
 	putchar('\n');
-
-    return 0;
+	return (0);
 }
