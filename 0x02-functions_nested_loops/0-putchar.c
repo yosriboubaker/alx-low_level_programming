08aea70fce387e7beb1527a5fb897f21c0e4466f
yosri boubaker <yosribbk1996@gmail.com>
2023-02-21 09:02:46 -0800

    first commit task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
