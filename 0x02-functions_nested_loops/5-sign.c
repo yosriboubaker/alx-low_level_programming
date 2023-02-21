a321ced6ac87061527745e6240770e25cb2f1ab6
yosri boubaker <yosribbk1996@gmail.com>
2023-02-21 15:19:51 -0800

    first commit task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_sign - check description
+ *
+ * Description: Check if n is signe or not
+ *
+ * Return: 1 if positive , 0 if n egale a zero , -1 if n negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	_putchar('\n');
+}
