187ba4dd09a9e60368ef64f87011e36bd2ca3d98
yosri boubaker <yosribbk1996@gmail.com>
2023-02-21 16:30:30 -0800

    first commit task 9

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,48 +1,11 @@
 #include "main.h"
 /**
- * times_table - print multiplication table
+ * add - sum of two integer number
+ * @x: first int
+ * @y: second int
+ * Return: a + b
  */
-
-void times_table(void)
+int add(int x, int y)
 {
-	int i, j, k;
-
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			k = i * j;
-
-			if ((k / 10) == 0)
-			{
-				if (j != 0)
-				{
-					_putchar(' ');
-				}
-
-				_putchar(k + '0');
-
-				if (j == 9)
-				{
-					continue;
-				}
-
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-
-				if (j == 9)
-				{
-					continue;
-				}
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
+	return (x + y);
 }
