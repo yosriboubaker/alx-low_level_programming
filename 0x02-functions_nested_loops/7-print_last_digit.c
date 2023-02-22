2d8c431746d575f58cdf0d833980f848a583174b
yosri boubaker <yosribbk1996@gmail.com>
2023-02-21 16:03:07 -0800

    first commit task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - a function that prints the last digit of an integer
+ * @x: integer argument
+ * Return: the last digit of an integer
+ */
+int print_last_digit(int x)
+{
+	int y;
+
+	if (x < 0)
+	{
+		x = abs(x);
+	}
+
+	y = x % 10;
+
+	_putchar(y + '0');
+
+	return (y);
+}
