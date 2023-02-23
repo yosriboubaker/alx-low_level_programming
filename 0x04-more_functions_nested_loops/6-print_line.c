2ef80f6c5426f46f63d3071b854824a2720b8e05
yosri boubaker <yosribbk1996@gmail.com>
2023-02-23 12:45:32 -0800

    betty style correcte in task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -13,9 +13,16 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		    _putchar('-');
+		_putchar('\n');
 	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('-');
+		}
 	_putchar('\n');
+	}
 }
