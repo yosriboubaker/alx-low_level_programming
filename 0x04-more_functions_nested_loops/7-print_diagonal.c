2b35bb08568431d09a96596130119e0496587c44
yosri boubaker <yosribbk1996@gmail.com>
2023-02-23 15:29:21 -0800

    first commmit for task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_diagonal - check description
+ * @n: integer parametre
+ *
+ * Description: draws diagonal
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	}
+}
