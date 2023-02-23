229ff4064ba38b683e6c1506f3cfecc4d9050d19
yosri boubaker <yosribbk1996@gmail.com>
2023-02-23 12:47:28 -0800

    betty style correcte in task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,28 +1,23 @@
-#include <stdio.h>
-#include <ctype.h>
 #include "main.h"
+
 /**
- * print_line - check description
- * @n: integer parametre
- *
- * Description: prints n  lines
- *
- * Return: void
+ * print_line - draws a straight line
+ * @n: number of times the character _ should be printed
  */
 void print_line(int n)
 {
-	int i;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
-		for (i = 0; i < n; i++)
+		int i;
+
+		for (i = 1; i <= n; i++)
 		{
-			_putchar('-');
+			_putchar('_');
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
+
 }
