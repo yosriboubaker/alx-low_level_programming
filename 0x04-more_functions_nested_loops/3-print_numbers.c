e60a6ab63474e88ceecc57c3c2f6d5ba16a6efb2
yosri boubaker <yosribbk1996@gmail.com>
2023-02-23 10:25:52 -0800

    first commit task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_numbers - check description
+ *
+ * Description: pirint numbers between 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
