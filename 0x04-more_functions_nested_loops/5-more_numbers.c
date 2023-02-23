c52a0da399650df0c123ba4750c3d826a6b80667
yosri boubaker <yosribbk1996@gmail.com>
2023-02-23 11:25:17 -0800

    first commit task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * more_numbers - check description
+ *
+ * Description: prints 10 times the numbers, from 0 to 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			printf("%d", j);
+		}
+		printf("\n");	
+	}
+	_putchar('\n');
+}
