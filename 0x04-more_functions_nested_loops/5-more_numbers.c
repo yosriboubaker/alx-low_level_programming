188500cabd9758d6f7977ced4f8a60341c5e2136
yosri boubaker <yosribbk1996@gmail.com>
2023-02-23 11:40:10 -0800

    first commit task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,9 +17,10 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			printf("%d", j);
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
-		printf("\n");
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
