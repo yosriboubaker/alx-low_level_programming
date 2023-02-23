4c1d076726424e4f4c53daf13149ba6c2d848268
yosri boubaker <yosribbk1996@gmail.com>
2023-02-23 10:05:48 -0800

    first commit of code task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * mul - multiplication of two integer
+ *@a: is the first integer parametre
+ *@b: is the second integer parametre
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+
+	return (m);
+}
