b629239f59fb0e8cfc4e7ee171909ce2fd0faaa0
yosri boubaker <yosribbk1996@gmail.com>
2023-02-23 16:20:12 -0800

    first commit task 9 FizzBizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+main()
+{
+	int i, j;
+	
+	for (i = 1; i <= 100; i++ )
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+		printf("%d ", i);
+		}
+	}
+	printf("\n");
+}
