b885c19b819c0c7ff284479cc5c5806d98c65cea
yosri boubaker <yosribbk1996@gmail.com>
2023-02-23 16:23:41 -0800

    first commit task 9 FizzBizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,20 +8,20 @@ main()
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			_putcahr("FizzBuzz ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			_putchar("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			_putchar("Buzz ");
 		}
 		else
 		{
-		printf("%d ", i);
+		printf("%d", i);
 		}
 	}
-	printf("\n");
+	_putchar('\n');
 }
