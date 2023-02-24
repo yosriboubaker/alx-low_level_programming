ba38e65576cd2b2e93dcb3e64b1a89f85278bae2
yosri boubaker <yosribbk1996@gmail.com>
2023-02-23 16:43:05 -0800

    second commit task 0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -4,23 +4,21 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
-int positive_or_negative(int i)
-{
 
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i < 0)
-	{
-		printf("%d is negative\n", i);
-	}
-	else
+void positive_or_negative(int i)
+{
+	if (i == 0)
 	{
 		printf("%d is zero\n", i);
 	}
-
-	return(0);
+	else if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
 }
