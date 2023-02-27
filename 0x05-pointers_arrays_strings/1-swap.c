05dda698abab2f0c6c4d52dc10f35b9a8f131b67
yosri boubaker <yosribbk1996@gmail.com>
2023-02-27 12:13:34 -0800

    first commit task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: a pointer the integer we want to set to 42
+ * @b: a pointer the integer we want to set to 98
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
