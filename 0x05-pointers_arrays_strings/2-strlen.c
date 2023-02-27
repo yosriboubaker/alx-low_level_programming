e49bc887b05941b09644b9030dbdb63359097ad0
yosri boubaker <yosribbk1996@gmail.com>
2023-02-27 12:39:12 -0800

    first commit task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * modif_my_param - set the integer to 402
+ * @m: a pointer the integer we want to set to 402
+ *
+ * Return: nothing
+ */
+int _strlen(char *s) 
+{	
+	return(strlen(s));
+}
