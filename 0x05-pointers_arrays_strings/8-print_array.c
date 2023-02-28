b5ff79c981670ffc2359bfcc4c393f3d5641f8c9
yosri boubaker <yosribbk1996@gmail.com>
2023-02-27 16:25:49 -0800

    first commit task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints array elements
+ *
+ * @a: array pointer
+ * @n: array length
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	putchar('\n');
+}
