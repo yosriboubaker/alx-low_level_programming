18a2f42b2743108a3dd7ae40bd512e535a4186c7
yosri boubaker <yosribbk1996@gmail.com>
2023-03-01 08:42:49 -0800

    first commit task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "string.h"
+#include "main.h"
+/**
+ * *_strcat - concat two strings
+ *
+ * @dest: the first char
+ * @src: the second char
+ *
+ * Return: the concat of two strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+C - pointer , array and strings
