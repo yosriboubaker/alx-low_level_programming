100828f54131a7708afabf6c10465f7c7bcd1d69
yosri boubaker <yosribbk1996@gmail.com>
2023-03-02 07:50:18 -0800

    second commit task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,16 +1,23 @@
 #include "main.h"
 #include <ctype.h>
 
+/**
+ * string_toupper - changes all lowercase to uppercase
+ *
+ * @stmt: the string
+ *
+ * Return: string
+ */
 char *string_toupper(char *stmt)
 {
 	int i;
-	for (i=0; stmt[i]; i++)
+
+	for (i = 0; stmt[i]; i++)
 	{
 		if (islower(stmt[i]))
 		{
 			stmt[i] = toupper(stmt[i]);
 		}
 	}
-	return(stmt);
+	return (stmt);
 }
-	
