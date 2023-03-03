510cb42f4b594d87e17ca4a3449face2c816ebe6
yosri boubaker <yosribbk1996@gmail.com>
2023-03-03 06:14:06 -0800

    second commit task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,32 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
 #include "main.h"
+#include <ctype.h>
 
 /**
  * cap_string - capitalizes all words of a string
- *
- * @s: string
- *
- * Return: string
+ * @s: the string
+ * Return: pointer to s
  */
 
 char *cap_string(char *s)
 {
-	int i;
+	int i, next = 1;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0 || isspace(s[i - 1]))
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == ',' ||
+			s[i] == ';' || s[i] == '.' || s[i] == '\n'
+			|| s[i] == '!' || s[i] == '?' || s[i] == '"' ||
+			s[i] == '(' || s[i] == ')' ||
+			s[i] == '{' || s[i] == '}')
+		{
+			next = 1;
+
+		} else if (next)
 		{
 			s[i] = toupper(s[i]);
+			next = 0;
 		}
 	}
+
 	return (s);
 }
