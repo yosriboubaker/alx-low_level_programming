8ed8061b23329e18e15206fcff3d5a344bd43962
yosri boubaker <yosribbk1996@gmail.com>
2023-03-08 12:37:57 -0800

    first commit task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: the string to check
+ *
+ * Return: length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
