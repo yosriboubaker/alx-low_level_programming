e9c487609b0952bee4e2bf666665cc86e79e0a72
yosri boubaker <yosribbk1996@gmail.com>
2023-03-10 15:29:16 -0800

    first commit task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - entry point & prints the program name
+ * @argc: Argument Count
+ * @argv: Argument Vector
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
