8a9f9bcf8f6cc6c8d2d7fe314ba89ffb51ce35d2
yosri boubaker <yosribbk1996@gmail.com>
2023-03-20 10:26:20 -0700

    first commit a code that prints the name of the file

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include "string.h"
+/**
+ * main - prints the file name
+ * Returns: 0
+ *
+ */
+int main(void)
+{
+	printf("%s\n",__FILE__);
+	return(0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+#include "string.h"
+/**
+ * main - prints the file name
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
